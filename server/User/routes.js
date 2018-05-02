dc7f8f0aab5f0eb8fc0f4b5f2ea6e30109ba0f9e
geni429 <geni429@gmail.com>
2018-05-02 16:59:52 +0900

    create session

--- server/User/routes.js
+++ server/User/routes.js
@@ -2,7 +2,7 @@
 
 const User = require('../../models/user');
 
-const signUp = async function(request, h) {
+const signUp = async (request, h) => {
   let user = new User();
   
   user.id = request.payload.id;
@@ -17,16 +17,28 @@ const signUp = async function(request, h) {
   }
 }
 
-const signIn = async function(request, h) {
-  User.findOne({ id: request.payload.id })
-    .then(result => {
-      console.log(reslut);
-      return h.response('User sign in success').code(201);
-    })
-    .catch(err => {
-      console.log(err);
-      return h.response('User sign in fail').code(400);
-    })
+const signIn = async (request, h) => {
+  try {
+    let result = await User.findOne({ id: request.payload.id, pw: request.payload.pw });
+    if (!result) return h.response('id or password is wrong.').code(401);
+
+    let cookie = request.state.session;
+
+    if (!cookie) {
+      cookie = {
+        username: 'geni',
+        firstVisit: false
+      }
+    }
+
+    cookie.lastVisit = Date.now();
+    console.log(`I will send this cookie: ${cookie}`);
+    
+    return h.response('User sign in success').state('session', cookie);
+  } catch(e) {
+    console.log(e);
+    return h.response('User sign in fail').code(500);
+  }
 }
 
 module.exports = [
@@ -38,6 +50,12 @@ module.exports = [
   {
     method: 'POST',
     path: '/signin',
-    handler: signIn
+    config: {
+      state: {
+        parse: true,
+        failAction: 'error'
+      },
+      handler: signIn
+    }
   }
 ]
--- server/server.js
+++ server/server.js
@@ -22,19 +22,39 @@ db.once('open', () => {
 
 mongoose.connect('mongodb://localhost/6_o_clock');
 
+server.route({
+  method: 'GET',
+  path: '/',
+  handler: function(request, h) {
+    return h.file(path.resolve(__dirname, '..', 'public/index.html'));
+  }
+});
+
+server.route({
+  method: 'GET',
+  path: '/session',
+  handler: function(request, h) {
+    console.log('session check');
+    const cookie = request.state.session;
+    console.log(`I got this cookie: ${cookie}`);
+    
+    if (!cookie) return h.response('none exist session').code(400);
+    return h.response('exist session').code(200);
+  }
+});
+
+server.route(user);
+
 const start = async () => {
   await server.register(require('inert'));
-
-  server.route({
-    method: 'GET',
-    path: '/',
-    handler: function(request, h) {
-      return h.file(path.resolve(__dirname, '..', 'public/index.html'));
-    }
-  })
-
-  server.route(user);
-
+  await server.state('session', {
+    ttl: null,
+    isSecure: true,
+    isHttpOnly: true,
+    encoding: 'base64json',
+    clearInvalid: false, // remove invalid cookies
+    strictHeader: true // don't allow violations of RFC 6265
+  });
   await server.start();
   console.log(`Server running at ${server.info.uri}`);
 }
